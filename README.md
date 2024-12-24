e8c8f2a6b288e3490e77231741901c95dd889c04
Ravindra Singh <rathoreravindra1026@gmail.com>
2024-12-24 08:39:22 +0530

    Readme.md added

---
+++ README.md
@@ -0,0 +1,1 @@
+### This selenium related repo
--- src/test/java/org/example/SeleniumFirst.java
+++ src/test/java/org/example/SeleniumFirst.java
@@ -1,4 +1,39 @@
 package org.example;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.Test;
+
 public class SeleniumFirst {
+
+    @Test
+    public void  testMethod01() throws InterruptedException {
+        EdgeOptions edgeOptions = new EdgeOptions();
+        edgeOptions.addArguments("--start-maximized");
+
+        WebDriver driver = new EdgeDriver(edgeOptions);
+        driver.get("https://katalon-demo-cura.herokuapp.com/");
+
+        WebElement makeAppointmentUrl = driver.findElement(By.id("btn-make-appointment"));
+        makeAppointmentUrl.click();
+
+        WebElement userName = driver.findElement(By.id("txt-username"));
+        userName.sendKeys("John Doe");
+
+        WebElement password = driver.findElement(By.id("txt-password"));
+        password.sendKeys("ThisIsNotAPassword");
+
+        Thread.sleep(4000);
+
+
+
+
+
+        driver.quit();
+
+
+    }
 }
